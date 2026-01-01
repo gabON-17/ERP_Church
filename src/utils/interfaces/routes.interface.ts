1c47ecd6679d9ae9985c126ba423d7c69c691241
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-01 17:04:24 -0300

    fetch: Foi adicionado o atributo endPoints na interface

--- src/utils/interfaces/routes.interface.ts
+++ src/utils/interfaces/routes.interface.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { endPoits } from "../types/endPoints.type";
 
 export interface Routes {
+  endPoints: endPoits;
   routing(): void;
   getRouter(): Router;
 }
