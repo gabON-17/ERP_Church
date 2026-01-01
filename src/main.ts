c0a99c314d13d36fec685a15162d717dd4c32a6e
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-01 17:03:00 -0300

    fetch: Adição de loggers na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response, Router } from "express";
+
 import { UsersController } from "./controller/users.controller";
 import { UsersRouter } from "./routes/users.route";
+
+import { logger } from "./logger/logger.config";
+import PinoHttp from "pino-http";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+// app.use(PinoHttp({ logger, autoLogging: true }));
 
 const usersRouter: UsersRouter = new UsersRouter(
   new UsersController(),
@@ -15,6 +22,8 @@ const usersRouter: UsersRouter = new UsersRouter(
 usersRouter.routing();
 app.use("/users", usersRouter.getRouter());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT || 8080, () => {
+  logger.info(
+    `Servidor rodando em http://localhost:${process.env.PORT || 8080}`
+  );
 });
