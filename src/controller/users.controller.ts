ef54ba03d7b1e2c40a5c436875a442e71a045dbf
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-01 17:09:22 -0300

    Montagem básica do controller

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
+import { logger } from "../logger/logger.config";
 
 export class UsersController {
-  findOne(req: Request, res: Response): void {}
-  findAll(req: Request, res: Response): void {
-    res.send("Hello Word");
+  findOne(req: Request, res: Response): void {
+    console.log(req.params);
+    res.status(200).json({ message: "findOne" });
   }
-  create(req: Request, res: Response): void {}
+
+  findAll(req: Request, res: Response): void {
+    res.status(200).json({ message: "findAll" });
+  }
+
+  create(req: Request, res: Response) {
+    return res.status(201).json({ message: "Test" });
+  }
 }
