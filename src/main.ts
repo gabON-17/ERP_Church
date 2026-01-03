afdfc2d15481b72165f876116905a6beb96ab64f
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-03 14:07:52 -0300

    fix: Alteração na configuração do PINO-HTTP(logger)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
 
 import { UsersController } from "./controller/users.controller";
 import { UsersRouter } from "./routes/users.route";
@@ -7,15 +7,43 @@ import { logger } from "./logger/logger.config";
 import PinoHttp from "pino-http";
 
 import dotenv from "dotenv";
+import { UsersService } from "./service/users.service";
+import { UsersRepository } from "./model/users.model";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-// app.use(PinoHttp({ logger, autoLogging: true }));
+app.use(
+  PinoHttp({
+    logger,
+    customSuccessMessage: (req, res) => {
+      return `Request feita: ${req.method} ${req.url}`;
+    },
+    serializers: {
+      req: (req: Request) => ({
+        id: req.id,
+        method: req.method,
+        host: req.host,
+        url: req.url,
+        userAgent: req.headers["user-agent"],
+        body: req.body,
+      }),
+      res: (res: Response) => ({
+        statusCode: res.statusCode,
+        body: res.jsonp,
+      }),
+    },
+
+    redact: {
+      paths: ["req.headers.authorization", "req.body.password"],
+      remove: true,
+    },
+  })
+);
 
 const usersRouter: UsersRouter = new UsersRouter(
-  new UsersController(),
+  new UsersController(logger, new UsersService(new UsersRepository())),
   Router()
 );
 
