9b2bc21cb6e7659a9b17f86a6f4990aacd4c90f6
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-03 14:09:49 -0300

    fetch: Foi criado um 'banco de dados' local para testes

--- src/model/users.model.ts
+++ src/model/users.model.ts
@@ -1,10 +1,37 @@
 import { UserEntity } from "../entitys/user.entity";
+import { logger } from "../logger/logger.config";
+import { UserDTO } from "../utils/dtos/user.dto";
 
 export class UsersRepository {
   private id: number = 0;
-  private users_list: UserEntity[];
+  private users_list: Array<UserEntity> = [];
 
-  create(user: UserEntity) {
-    this.users_list.push(user);
+  async create(user: UserDTO): Promise<void | unknown> {
+    try {
+      let newUser: UserEntity | void = {
+        id: `${this.id}`,
+        ...user,
+      };
+
+      if (typeof newUser == "undefined") {
+        throw new Error("Usuário indefinido, não é possível criar");
+      }
+
+      this.users_list.push(newUser);
+      this.id++;
+
+      logger.info("Usuario CRIADO");
+    } catch (e) {
+      logger.error("Ocorreu um error na criação do usuário");
+      logger.debug("Retornando o ERROR para o SERVICE");
+
+      return e;
+    }
+  }
+
+  async findAll(): Promise<UserEntity[]> {
+    return this.users_list;
   }
 }
+
+export const userRepository = new UsersRepository();
