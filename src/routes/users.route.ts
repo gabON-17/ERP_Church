bc221143db3aed852b6eb8f4060b9ca8ac554086
Gabriel Henrique <gabrielbarros1704@gmail.com>
2026-01-03 14:04:37 -0300

    fetch: Middleware colocado na rota POST url: /create

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,9 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { Routes } from "../utils/interfaces/routes.interface";
 import { endPoits } from "../utils/types/endPoints.type";
 import { UsersController } from "../controller/users.controller";
+import { VerifyDtoMiddleware } from "../utils/middlewares/verifyDTO.middleware";
+import { UserDTO } from "../utils/dtos/user.dto";
 
 export class UsersRouter implements Routes {
   constructor(
@@ -18,18 +20,22 @@ export class UsersRouter implements Routes {
   routing(): void {
     this.router.get(this.endPoints.get_routes[0], this.findAll.bind(this));
     this.router.get(this.endPoints.get_routes[1], this.findOne.bind(this));
-    this.router.post(this.endPoints.post_routes[0], this.create.bind(this));
+    this.router.post(
+      this.endPoints.post_routes[0],
+      VerifyDtoMiddleware.verifyDTO,
+      this.create.bind(this)
+    );
   }
 
-  private findAll(req: Request, res: Response): void {
+  private findAll(req: Request, res: Response, next: NextFunction): void {
     this.usersController.findAll(req, res);
   }
 
-  private findOne(req: Request, res: Response): void {
+  private findOne(req: Request, res: Response, next: NextFunction): void {
     this.usersController.findOne(req, res);
   }
 
-  private create(req: Request, res: Response): void {
+  private create(req: Request, res: Response, next: NextFunction): void {
     this.usersController.create(req, res);
   }
 
